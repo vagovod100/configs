565536a5839ec700ae3049897a028c4397f56cf3
vagovod100 <nadgob358@gmail.com>
2026-04-09 12:04:37 +0300

    add fish config

---
+++ fish/functions/l.fish
@@ -0,0 +1,3 @@
+function l --wraps='eza --icons -la' --description 'alias l eza --icons -la'
+    eza --icons -la $argv
+end
---
+++ fish/functions/zz.fish
@@ -0,0 +1,3 @@
+function zz --wraps='cd;clear' --description 'alias zz=cd;clear'
+    cd;clear $argv
+end
